9e91043ed774e18825e61115d4bfd3e501529b5a
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-20 17:38:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
-
+let main=document.getElementById("main");
+let orderList=document.getElementById("infi-list");
+let ct=1;
+for(let i=1; i<=10; i++){
+	let list = document.createElement('li');
+        list.innerText = `Item ${ct++}`;
+        orderList.append(list);
+}
+orderList.addEventListener("scroll", function(){
+    let threshold = 2; 
+    if (orderList.scrollHeight - orderList.scrollTop >= orderList.clientHeight + threshold) {
+        let list = document.createElement('li');
+        list.innerText = `Item ${ct++}`;
+        orderList.append(list);
+    }
+});
